4333e8634dc8e8b7c918a1ed3cc488c21f60ebef
Olaf Klischat <olaf.klischat@sofd.de>
2009-10-19 10:32:00 +0000

    copyUiStateToSubComponents stuff, used for foreground/background color for now

--- src/de/sofd/viskit/ui/imagelist/JImageListView.java
+++ src/de/sofd/viskit/ui/imagelist/JImageListView.java
@@ -4,6 +4,10 @@ import de.sofd.util.BiIdentityHashMap;
 import de.sofd.util.BiMap;
 import de.sofd.util.IdentityHashSet;
 import de.sofd.util.Misc;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.event.ContainerAdapter;
+import java.awt.event.ContainerEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
@@ -64,6 +68,11 @@ public abstract class JImageListView extends JPanel {
     //       in the model more than once (we kind of do that for now; see the modelChangeListener below)
     private BiMap<ImageListViewModelElement, ImageListViewCell> cellsByElementMap
             = new BiIdentityHashMap<ImageListViewModelElement, ImageListViewCell>();
+
+    public JImageListView() {
+        ensureUiStateIsCopiedForAddedComponents();
+    }
+
 
     /**
      * Get the value of model
@@ -475,6 +484,58 @@ public abstract class JImageListView extends JPanel {
         }
     }
 
+    @Override
+    public void setForeground(Color fg) {
+        super.setForeground(fg);
+        copyUiStateToSubComponents();
+    }
+
+
+    @Override
+    public void setBackground(Color bg) {
+        super.setBackground(bg);
+        copyUiStateToSubComponents();
+    }
+
+
+    protected void copyUiStateToSubComponents() {
+        for (Component c : this.getComponents()) {
+            copyUiStateToSubComponent(c);
+        }
+    }
+
+    /**
+     * The base class (JImageListView) calls this whenever a UI state like
+     * foreground/background colors should to be copied from this component
+     * to a child component (c). This happens when (a) such a UI state
+     * was changed or (b) when a new child component was added. JImageListView
+     * ensures that this is done correctly ((b) is ensured by the constructor
+     * calling {@link #ensureUiStateIsCopiedForAddedComponents() }). The default
+     * implementation copies the foreground and background color. Subclasses
+     * may override to copy additional properties.
+     *
+     * @param c
+     */
+    protected void copyUiStateToSubComponent(Component c) {
+        c.setForeground(this.getForeground());
+        c.setBackground(this.getBackground());
+    }
+
+    /**
+     * Ensures {@link #copyUiStateToSubComponent(java.awt.Component) } will
+     * be called for any child component added to this component in the
+     * future. Called once by the default constructor of JImageListView.
+     * Subclasses may (rarely) override, e.g. with an empty implementation
+     * if they want to inhibit this behaviour for some (strange) reason.
+     */
+    protected void ensureUiStateIsCopiedForAddedComponents() {
+        this.addContainerListener(new ContainerAdapter() {
+            @Override
+            public void componentAdded(ContainerEvent e) {
+                JImageListView.this.copyUiStateToSubComponent(e.getChild());
+            }
+        });
+    }
 
     private PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
 
