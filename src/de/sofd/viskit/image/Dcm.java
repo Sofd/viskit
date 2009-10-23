7e6431042352895bf7d922c144dcfb831aa64fc9
Fokko Breden <fokko.breden@sofd.de>
2009-10-23 09:29:34 +0000

    improved

--- src/de/sofd/viskit/image/Dcm.java
+++ src/de/sofd/viskit/image/Dcm.java
@@ -1,13 +1,16 @@
 package de.sofd.viskit.image;
 
 import java.net.URL;
+import org.apache.log4j.Logger;
 import org.dcm4che2.data.DicomObject;
+import org.dcm4che2.data.Tag;
 
 /**
  *
  */
 public class Dcm {
 
+    static final Logger log4jLogger = Logger.getLogger(Dcm.class);
     DicomObject dicomObject;
     URL url;
 
@@ -28,5 +31,25 @@ public class Dcm {
 
     public void setUrl(URL url) {
         this.url = url;
+    }
+
+    public Integer getWindowWidthTag() {
+        Integer result = null;
+        try {
+            result = (int) dicomObject.getFloat(Tag.WindowWidth);
+        } catch (Exception ex) {
+            log4jLogger.error("getWindowWidthTag", ex);
+        }
+        return result;
+    }
+
+    public Integer getDicomWindowCenterTag() {
+        Integer result = null;
+        try {
+            result = (int) dicomObject.getFloat(Tag.WindowCenter);
+        } catch (Exception ex) {
+            log4jLogger.error("getDicomWindowCenterTag", ex);
+        }
+        return result;
     }
 }
