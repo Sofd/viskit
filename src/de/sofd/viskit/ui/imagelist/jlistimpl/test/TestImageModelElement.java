af3bea9a19bcbf481aff4920bc9aab1200b0f158
Olaf Klischat <olaf.klischat@sofd.de>
2009-11-19 19:45:04 +0000

    viskit: TestImageModelElement rebased on a DICOM image from one of the test series (so it can be windowed, which the previous image couldn't)

--- src/de/sofd/viskit/ui/imagelist/jlistimpl/test/TestImageModelElement.java
+++ src/de/sofd/viskit/ui/imagelist/jlistimpl/test/TestImageModelElement.java
@@ -3,22 +3,22 @@ package de.sofd.viskit.ui.imagelist.jlistimpl.test;
 import de.sofd.draw2d.Drawing;
 import de.sofd.draw2d.EllipseObject;
 import de.sofd.draw2d.RectangleObject;
-import de.sofd.viskit.ui.imagelist.ImageListViewModelElement;
+import de.sofd.viskit.test.FileBasedDicomImageListViewModelElement;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
-import javax.imageio.ImageIO;
 
 /**
  *
  * @author olaf
  */
-public class TestImageModelElement implements ImageListViewModelElement {
+public class TestImageModelElement extends FileBasedDicomImageListViewModelElement {
 
     private static BufferedImage image;
     private final Drawing roiDrawing;
 
     public TestImageModelElement(int number) {
-        roiDrawing = new Drawing();
+        setUrl(this.getClass().getResource("/de/sofd/viskit/test/resources/series/series2/cd014__center001__24.dcm"));
+        roiDrawing = getRoiDrawing();
         RectangleObject rect = new RectangleObject();
         rect.setLocation(50, 30, 200, 125);
         rect.setColor(Color.RED);
@@ -34,23 +34,6 @@ public class TestImageModelElement implements ImageListViewModelElement {
         roiDrawing.addDrawingObject(rect);
         roiDrawing.addDrawingObject(ellipse);
         roiDrawing.addDrawingObject(ell2);
-    }
-
-    @Override
-    public BufferedImage getImage() {
-        if (null == image) {
-            try {
-                image = ImageIO.read(this.getClass().getResourceAsStream("mri_brain_small.jpg"));
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return image;
-    }
-
-    @Override
-    public Drawing getRoiDrawing() {
-        return roiDrawing;
     }
 
 }
