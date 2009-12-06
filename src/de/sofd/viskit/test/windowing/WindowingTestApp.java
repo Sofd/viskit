cb079b2c3850d127abeebc55679d0a9e00e24d67
Olaf Klischat <olaf.klischat@sofd.de>
2009-12-06 16:06:02 +0000

    viskit: windowing test app: more statistics on mouse click in image; saving of images; more test images (from http://www.barre.nom.fr/medical/samples/)

--- src/de/sofd/viskit/test/windowing/WindowingTestApp.java
+++ src/de/sofd/viskit/test/windowing/WindowingTestApp.java
@@ -5,9 +5,12 @@ import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.swing.AbstractAction;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -23,7 +26,7 @@ import org.dcm4che2.io.DicomInputStream;
 public class WindowingTestApp {
 
     final JDicomObjectImageViewer viewer;
-    JLabel wlLabel, wwLabel;
+    JLabel wlLabel, wwLabel, pixelLabel;
 
     public WindowingTestApp(URL url) throws Exception {
         DicomInputStream din = new DicomInputStream(url.openStream());
@@ -41,21 +44,34 @@ public class WindowingTestApp {
         toolbar.setFloatable(false);
         toolbar.add(wlLabel = new JLabel());
         toolbar.add(wwLabel = new JLabel());
+        //toolbar.add(pixelLabel = new JLabel());
         MouseAdapter windowingCellMouseListener = new MouseAdapter() {
             private Point prevPoint;
             @Override
             public void mousePressed(MouseEvent e) {
                 prevPoint = e.getPoint();
                 System.out.println("unscaled point: " + prevPoint);
-                System.out.println("image point: " + prevPoint.x / viewer.getDynamicZoomFactor() + ", " + prevPoint.y / viewer.getDynamicZoomFactor());
+                double x = prevPoint.x / viewer.getDynamicZoomFactor();
+                double y = prevPoint.y / viewer.getDynamicZoomFactor();
+                System.out.println("image point: " + x + ", " + y + "; raw pixel: " +
+                                   valsToString(viewer.getRawObjectImage().getRaster().getPixel((int)x, (int)y, (int[])null)) + "; windowed pixel: " +
+                                   valsToString(viewer.getWindowedObjectImage().getRaster().getPixel((int)x, (int)y, (int[])null)));
                 e.consume();
+            }
+            private String valsToString(int[] vals) {
+                StringBuffer result = new StringBuffer("[");
+                for (int val : vals) {
+                    result.append(val + " ");
+                }
+                result.append("]");
+                return result.toString();
             }
             @Override
             public void mouseDragged(MouseEvent e) {
                 Point p = e.getPoint();
                 if (prevPoint != null) {
-                    viewer.setWindowingParams(viewer.getWindowLocation() + p.x - prevPoint.x,
-                                              viewer.getWindowWidth() + p.y - prevPoint.y);
+                    viewer.setWindowingParams(viewer.getWindowLocation() + 1 * (p.x - prevPoint.x),
+                                              viewer.getWindowWidth() + 1 * (p.y - prevPoint.y));
                     updateWlWwLabels();
                 }
                 prevPoint = p;
@@ -77,10 +93,17 @@ public class WindowingTestApp {
                 updateWlWwLabels();
             }
         });
-        toolbar.add(new AbstractAction("Set to Zahn") {
+        toolbar.add(new AbstractAction("Set to 0..255") {
             @Override
             public void actionPerformed(ActionEvent e) {
-                viewer.setWindowingParams(13466, 38603);
+                viewer.setWindowingParams(128, 255);
+                updateWlWwLabels();
+            }
+        });
+        toolbar.add(new AbstractAction("Set to 0..65535") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                viewer.setWindowingParams(32768, 65535);
                 updateWlWwLabels();
             }
         });
@@ -90,6 +113,26 @@ public class WindowingTestApp {
                 viewer.analyzeRawImage();
                 viewer.analyzeRawRaster();
                 viewer.analyzeWindowedImage();
+            }
+        });
+        toolbar.add(new AbstractAction("RawImgSave") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    ImageIO.write(viewer.getRawObjectImage(), "PNG", new File("/tmp/rawImg.png"));
+                } catch (IOException ex) {
+                    Logger.getLogger(WindowingTestApp.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+        toolbar.add(new AbstractAction("WindowedImgSave") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    ImageIO.write(viewer.getWindowedObjectImage(), "PNG", new File("/tmp/windowedImg.png"));
+                } catch (IOException ex) {
+                    Logger.getLogger(WindowingTestApp.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         });
         viewer.setWindowingParamsToDicom();
@@ -118,6 +161,14 @@ public class WindowingTestApp {
                     //URL url = new URL("file:///shares/shared/DICOM-Testbilder/24-bit J2K Lossy Color.dcm");
                     //URL url = new URL("file:///tmp/24-bit Uncompressed Color.dcm");
                     //URL url = new URL("file:///tmp/f0003563_00623.dcm");
+
+                    //URL url = this.getClass().getResource("/de/sofd/viskit/test/resources/16bit-test-imgs/CT-MONO2-16-ankle.dcm");
+                    //URL url = this.getClass().getResource("/de/sofd/viskit/test/resources/16bit-test-imgs/CT-MONO2-16-brain.dcm");
+                    //URL url = this.getClass().getResource("/de/sofd/viskit/test/resources/16bit-test-imgs/CT-MONO2-16-chest.dcm");
+                    //URL url = this.getClass().getResource("/de/sofd/viskit/test/resources/16bit-test-imgs/CT-MONO2-16-ort.dcm");
+                    //URL url = this.getClass().getResource("/de/sofd/viskit/test/resources/16bit-test-imgs/CT-MONO2-8-abdo.dcm");
+                    //URL url = this.getClass().getResource("/de/sofd/viskit/test/resources/16bit-test-imgs/MR-MONO2-12-angio-an1.dcm");
+
                     new WindowingTestApp(url);
                 } catch (Exception ex) {
                     Logger.getLogger(WindowingTestApp.class.getName()).log(Level.SEVERE, null, ex);
