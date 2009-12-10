a0b077568b2d3aa3960fdd43da7b0c6b516bccf1
Olaf Klischat <olaf.klischat@sofd.de>
2009-12-10 19:19:25 +0000

    viskit: GLImageListViewCellViewer continued

--- src/de/sofd/viskit/ui/imagelist/jlistimpl/GLImageListViewCellViewer.java
+++ src/de/sofd/viskit/ui/imagelist/jlistimpl/GLImageListViewCellViewer.java
@@ -12,7 +12,6 @@ import java.awt.image.BufferedImageOp;
 import javax.media.opengl.GL2;
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLContext;
-import javax.media.opengl.GLDrawable;
 import javax.media.opengl.GLEventListener;
 import javax.media.opengl.awt.GLJPanel;
 
@@ -24,17 +23,19 @@ import javax.media.opengl.awt.GLJPanel;
  */
 public class GLImageListViewCellViewer extends GLJPanel {
 
-    protected GLContext sharedContext;
+    protected static GLContext sharedContext;
 
     private final ImageListViewCell displayedCell;
 
     public GLImageListViewCellViewer(ImageListViewCell cell) {
         this.displayedCell = cell;
+        /*
         if (null == sharedContext) {
             sharedContext = this.getContext();
         } else {
             this.setContext(sharedContext);
         }
+        */
         this.addGLEventListener(new GLEventHandler());
     }
 
@@ -83,8 +84,6 @@ public class GLImageListViewCellViewer extends GLJPanel {
     }
 
 
-    // TODO: implement painting (with an internal GLEventListener rather than overriding paintComponent)
-
     protected void paintComponent_ren(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
         super.paintComponent(g2d);
@@ -117,31 +116,9 @@ public class GLImageListViewCellViewer extends GLJPanel {
         public void init(GLAutoDrawable glAutoDrawable) {
             System.out.println("INIT GL drawable: " + glAutoDrawable);
             GL2 gl = (GL2) glAutoDrawable.getGL();
-            gl.glMatrixMode(gl.GL_PROJECTION);
-            gl.glLoadIdentity();
-
-            /*
-            // isometric projection
-            GLdouble vpHeightInObjCoords = vpWidthInWorldCoords * vpHeight / vpWidth;
-            glOrtho(-vpWidthInWorldCoords/2,  //    GLdouble      left,
-                    vpWidthInWorldCoords/2,   //    GLdouble      right,
-                    -vpHeightInObjCoords/2, //    GLdouble      bottom,
-                    vpHeightInObjCoords/2,  //    GLdouble      top,
-                    -1000, //  GLdouble      nearVal,
-                    1000   //  GLdouble      farVal
-                    );
-
-
-            // eye coord -> viewport transformation
-            gl.glViewport(0, //GLint x,
-                          0, //GLint y,
-                          drawable.getWidth(), //GLsizei width,
-                          drawable.getHeight() //GLsizei height
-                          );
-            gl.glDepthRange(0,1);
-            */
-            
+            setupEye2ViewportTransformation(gl);
             gl.glClearColor(0,0,0,0);
+            gl.glShadeModel(gl.GL_FLAT);
         }
 
         @Override
@@ -151,6 +128,19 @@ public class GLImageListViewCellViewer extends GLJPanel {
         @Override
         public void display(GLAutoDrawable glAutoDrawable) {
             GL2 gl = (GL2) glAutoDrawable.getGL();
+            gl.glClear(gl.GL_COLOR_BUFFER_BIT);
+            gl.glMatrixMode(gl.GL_MODELVIEW);
+            gl.glLoadIdentity();
+            gl.glTranslated(displayedCell.getCenterOffset().getX(), displayedCell.getCenterOffset().getY(), 0);
+            gl.glScaled(displayedCell.getScale(), displayedCell.getScale(), 0);
+            gl.glColor3f(0, 1, 0);
+            float w2 = (float) getOriginalImageWidth() / 2, h2 = (float) getOriginalImageHeight() / 2;
+            gl.glBegin(GL2.GL_QUADS);
+            gl.glVertex2f(-w2, h2);
+            gl.glVertex2f( w2,  h2);
+            gl.glVertex2f( w2, -h2);
+            gl.glVertex2f(-w2, -h2);
+            gl.glEnd();
             //glAutoDrawable.swapBuffers();
         }
 
@@ -158,7 +148,33 @@ public class GLImageListViewCellViewer extends GLJPanel {
         public void reshape(GLAutoDrawable glAutoDrawable, int x, int y, int width, int height) {
             GL2 gl = (GL2) glAutoDrawable.getGL();
             displayedCell.setLatestSize(getSize());
+            setupEye2ViewportTransformation(gl);
         }
+
+        private void setupEye2ViewportTransformation(GL2 gl) {
+            gl.glMatrixMode(gl.GL_PROJECTION);
+            gl.glLoadIdentity();
+            Dimension sz = displayedCell.getLatestSize();
+            /*
+            if (sz != null) {
+                gl.glOrtho(-sz.width / 2,   //    GLdouble      left,
+                            sz.width / 2,   //    GLdouble      right,
+                           -sz.height / 2,  //    GLdouble      bottom,
+                            sz.height / 2,  //    GLdouble      top,
+                           -1000, //  GLdouble      nearVal,
+                            1000   //  GLdouble      farVal
+                           );
+
+                gl.glViewport(0, //GLint x,
+                              0, //GLint y,
+                              getWidth(), //GLsizei width,
+                              getHeight() //GLsizei height
+                              );
+                gl.glDepthRange(0,1);
+            }
+             */
+        }
+
     };
 
 }
