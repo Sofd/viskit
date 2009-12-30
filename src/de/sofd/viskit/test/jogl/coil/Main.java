732ed4945af76c6d71361b90628af0de8430acdf
Olaf Klischat <olaf.klischat@sofd.de>
2009-12-30 01:05:27 +0000

    viskit: JOGL coils test: add combo box to check GLCanvas/swing interoperability

--- src/de/sofd/viskit/test/jogl/coil/Main.java
+++ src/de/sofd/viskit/test/jogl/coil/Main.java
@@ -1,12 +1,15 @@
 package de.sofd.viskit.test.jogl.coil;
 
 import com.sun.opengl.util.Animator;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JToolBar;
 import javax.swing.SwingUtilities;
 
 /**
@@ -19,8 +22,13 @@ public class Main {
         JFrame frame = new JFrame("Coil");
         //Frame frame = new Frame("Coil");
         frame.setBackground(Color.black);
+        JToolBar toolbar = new JToolBar();
+        toolbar.setFloatable(false);
+        JComboBox cb = new JComboBox(new Object[]{"foo","bar","baz","quux"});
+        toolbar.add(cb);
+        frame.add(cb, BorderLayout.NORTH);
         CoilViewer glViewer = new CoilViewer();
-        frame.add(glViewer);
+        frame.add(glViewer, BorderLayout.CENTER);
         frame.setSize(800, 600);
         frame.setBackground(Color.black);
         frame.setVisible(true);
