7afcba8e992f82f46b73daf3812c3266422e257d
Olaf Klischat <olaf.klischat@sofd.de>
2009-12-30 01:17:57 +0000

    viskit: JOGL coils test: multiple frames / GL canvasses; demonstrate that GL context sharing doesnt work yet

--- src/de/sofd/viskit/test/jogl/coil/Main.java
+++ src/de/sofd/viskit/test/jogl/coil/Main.java
@@ -19,28 +19,32 @@ import javax.swing.SwingUtilities;
 public class Main {
 
     public Main() {
-        JFrame frame = new JFrame("Coil");
-        //Frame frame = new Frame("Coil");
-        frame.setBackground(Color.black);
-        JToolBar toolbar = new JToolBar();
-        toolbar.setFloatable(false);
-        JComboBox cb = new JComboBox(new Object[]{"foo","bar","baz","quux"});
-        toolbar.add(cb);
-        frame.add(cb, BorderLayout.NORTH);
-        CoilViewer glViewer = new CoilViewer();
-        frame.add(glViewer, BorderLayout.CENTER);
-        frame.setSize(800, 600);
-        frame.setBackground(Color.black);
-        frame.setVisible(true);
-        final Animator anim = new Animator(glViewer.getGlCanvas());
+        int nFrames = 3;
+        final Animator anim = new Animator();
+        for (int i = 0; i < nFrames; i++) {
+            JFrame frame = new JFrame("Coil");
+            //Frame frame = new Frame("Coil");
+            frame.setBackground(Color.black);
+            JToolBar toolbar = new JToolBar();
+            toolbar.setFloatable(false);
+            JComboBox cb = new JComboBox(new Object[]{"foo","bar","baz","quux"});
+            toolbar.add(cb);
+            frame.add(cb, BorderLayout.NORTH);
+            CoilViewer glViewer = new CoilViewer();
+            anim.add(glViewer.getGlCanvas());
+            frame.add(glViewer, BorderLayout.CENTER);
+            frame.setSize(800, 600);
+            frame.setBackground(Color.black);
+            frame.setVisible(true);
+            frame.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    //anim.stop();
+                    System.exit(0);
+                }
+            });
+        }
         anim.start();
-        frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                //anim.stop();
-                System.exit(0);
-            }
-        });
     }
 
 
