5158f413f89fb4cbc1ef03411cc24ceaba435b52
Olaf Klischat <olaf.klischat@sofd.de>
2010-01-06 04:54:58 +0000

    viskit: GLImageListGLImageListViewCellViewer: started switch to JPanel / wrapped GLCanvas, mouse handling (intermediate state)

--- src/de/sofd/viskit/ui/imagelist/jlistimpl/GLImageListViewCellViewer.java
+++ src/de/sofd/viskit/ui/imagelist/jlistimpl/GLImageListViewCellViewer.java
@@ -3,23 +3,36 @@ package de.sofd.viskit.ui.imagelist.jlistimpl;
 import com.sun.opengl.util.texture.Texture;
 import com.sun.opengl.util.texture.TextureCoords;
 import com.sun.opengl.util.texture.awt.AWTTextureIO;
+import de.sofd.util.IdentityHashSet;
 import de.sofd.viskit.ui.imagelist.ImageListViewCell;
+import java.awt.AWTEvent;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GraphicsDevice;
+import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImageOp;
+import java.util.Set;
 import javax.media.opengl.DebugGL2;
 import javax.media.opengl.GL2;
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLCapabilities;
+import javax.media.opengl.GLCapabilitiesChooser;
 import javax.media.opengl.GLContext;
 import javax.media.opengl.GLEventListener;
 import javax.media.opengl.GLProfile;
+import javax.media.opengl.awt.GLCanvas;
 import javax.media.opengl.awt.GLJPanel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 /**
  * Swing component for displaying a {@link ImageListViewCell} using an OpenGL-based
@@ -27,33 +40,130 @@ import javax.media.opengl.awt.GLJPanel;
  *
  * @author olaf
  */
-public class GLImageListViewCellViewer extends GLJPanel {
+public class GLImageListViewCellViewer extends JPanel {
 
-    protected static GLContext sharedContext;
+    static {
+        System.setProperty("sun.awt.noerasebackground", "true");
+    }
+
+    private static final Set<GLImageListViewCellViewer> instances = new IdentityHashSet<GLImageListViewCellViewer>();
+
+    private static final SharedContextData sharedContextData = new SharedContextData();
 
     private final ImageListViewCell displayedCell;
     private Texture imageTexture;   // TODO: uniquely identifyable images, hash texture objects by them (LRU cache)
 
-    private static GLCapabilities caps;
-    static {
-        caps = new GLCapabilities(GLProfile.get(GLProfile.GL2));
-        caps.setAlphaBits(8);
+    private GLAutoDrawable glCanvas;
+
+    private class MouseEventEnableGLCanvas extends GLCanvas {
+
+        public MouseEventEnableGLCanvas(GLCapabilities capabilities, GLCapabilitiesChooser chooser, GLContext shareWith, GraphicsDevice device) {
+            super(capabilities, chooser, shareWith, device);
+            enableEvents(AWTEvent.MOUSE_EVENT_MASK);
+            enableEvents(AWTEvent.MOUSE_MOTION_EVENT_MASK);
+            enableEvents(AWTEvent.MOUSE_WHEEL_EVENT_MASK);
+        }
+
+        public MouseEventEnableGLCanvas(GLCapabilities capabilities) {
+            super(capabilities);
+            enableEvents(AWTEvent.MOUSE_EVENT_MASK);
+            enableEvents(AWTEvent.MOUSE_MOTION_EVENT_MASK);
+            enableEvents(AWTEvent.MOUSE_WHEEL_EVENT_MASK);
+        }
+
+        public MouseEventEnableGLCanvas() {
+            enableEvents(AWTEvent.MOUSE_EVENT_MASK);
+            enableEvents(AWTEvent.MOUSE_MOTION_EVENT_MASK);
+            enableEvents(AWTEvent.MOUSE_WHEEL_EVENT_MASK);
+        }
+
+        @Override
+        protected void processMouseEvent(MouseEvent e) {
+            super.processMouseEvent(e);
+            System.out.println("GLCanvas processMouseEvent " + e);
+            GLImageListViewCellViewer.this.dispatchEvent(e);
+            //GLImageListViewCellViewer.this.processMouseEvent(e);
+        }
+
+        @Override
+        protected void processMouseMotionEvent(MouseEvent e) {
+            super.processMouseMotionEvent(e);
+            //GLImageListViewCellViewer.this.processMouseMotionEvent(e);
+            GLImageListViewCellViewer.this.dispatchEvent(e);
+        }
+
+        @Override
+        protected void processMouseWheelEvent(MouseWheelEvent e) {
+            super.processMouseWheelEvent(e);
+            //GLImageListViewCellViewer.this.processMouseWheelEvent(e);
+            GLImageListViewCellViewer.this.dispatchEvent(e);
+        }
+
     }
 
     public GLImageListViewCellViewer(ImageListViewCell cell) {
-        super(caps);
         System.out.println("CREA GL drawable: " + this.hashCode());
         this.displayedCell = cell;
-        if (null == sharedContext) {
-            sharedContext = this.getContext();
-        } else {
-            this.setContext(sharedContext);
+        enableEvents(AWTEvent.MOUSE_EVENT_MASK);
+        enableEvents(AWTEvent.MOUSE_MOTION_EVENT_MASK);
+        enableEvents(AWTEvent.MOUSE_WHEEL_EVENT_MASK);
+        setLayout(new GridLayout(1, 1));
+        ///*
+        if (instances.isEmpty() || sharedContextData.getGlContext() != null) {
+            createGlCanvas();
         }
-        this.addGLEventListener(new GLEventHandler());
+        instances.add(this);
+        // */
+        //add(new ImageListViewCellViewer(cell));
+        //enableEvents(AWTEvent.MOUSE_EVENT_MASK);
+    }
+
+    private void createGlCanvas() {
+        if (getComponentCount() != 0) {
+            throw new IllegalStateException("trying to initialize GL canvas more than once");
+        }
+        GLCapabilities caps = new GLCapabilities(GLProfile.get(GLProfile.GL2));
+        caps.setDoubleBuffered(true);
+        glCanvas = new MouseEventEnableGLCanvas(caps, null, sharedContextData.getGlContext(), null);
+        glCanvas.addGLEventListener(new GLEventHandler());
+        Component glCanvasComp = (Component)glCanvas;
+        this.add(glCanvasComp);
+        //enableEvents(AWTEvent.MOUSE_EVENT_MASK);
+        MouseAdapter ma = new MouseAdapter() {
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                System.out.println("mousePressed");
+            }
+
+        };
+        //this.addMouseListener(ma);
+        //this.addMouseMotionListener(ma);
+        //this.addMouseWheelListener(ma);
+        //glCanvasComp.addMouseListener(ma);
+        //glCanvasComp.addMouseMotionListener(ma);
+        //glCanvasComp.addMouseWheelListener(ma);
+        revalidate();
+        //System.out.println("CREATED CANVAS " + getId(glCanvas) + " of viewer " + getId(this) + ", its context is now: " + getId(glCanvas.getContext()));
+        /*
+        for (Runnable1<WorldViewer> callback : glCanvasCreatedCallbacks) {
+            callback.run(this);
+        }
+        */
+    }
+
+    @Override
+    protected void processMouseEvent(MouseEvent e) {
+        System.out.println("GLImgCellViewer processMouseEvent " + e);
+        super.processMouseEvent(e);
+    }
+
+    public GLAutoDrawable getGlCanvas() {
+        return glCanvas;
     }
 
     // TODO: following method are copy & paste with ImageListeCellViewer...
-    // (can't use common superclass for that b/c there already are two different superclasses)
+    //       Use a common superclass!
 
     public ImageListViewCell getDisplayedCell() {
         return displayedCell;
@@ -97,22 +207,6 @@ public class GLImageListViewCellViewer extends GLJPanel {
     }
 
 
-    protected void paintComponent_ren(Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
-        super.paintComponent(g2d);
-
-        displayedCell.setLatestSize(getSize());
-
-        //give the render* methods a Graphics2D whose coordinate system
-        //(and eventually, clipping) is already relative to the area in
-        //which the image should be drawn
-        Graphics2D userGraphics = (Graphics2D)g2d.create();
-        Point2D imageOffset = getImageOffset();
-        userGraphics.transform(AffineTransform.getTranslateInstance(imageOffset.getX(), imageOffset.getY()));
-        renderImage(userGraphics);
-        renderOverlays(userGraphics);
-    }
-
     protected void renderImage(Graphics2D g2d) {
         BufferedImageOp scaleImageOp = new AffineTransformOp(getDicomToUiTransform(), AffineTransformOp.TYPE_BILINEAR);
         //g2d.drawImage(getWindowedImage(), scaleImageOp, 0, 0);
@@ -121,7 +215,6 @@ public class GLImageListViewCellViewer extends GLJPanel {
     protected void renderOverlays(Graphics2D g2d) {
         displayedCell.getRoiDrawingViewer().paint(g2d);
     }
-
 
     protected class GLEventHandler implements GLEventListener {
 
@@ -138,7 +231,20 @@ public class GLImageListViewCellViewer extends GLJPanel {
             gl.setSwapInterval(1);
             gl.glClearColor(0,0,0,0);
             gl.glShadeModel(gl.GL_FLAT);
-            //imageTexture = null;   // TODO: this should not work reliably now, but apparently does, and the TestTexture jogl demo does the same thing...
+            sharedContextData.ref(getGlCanvas().getContext());
+            if (sharedContextData.getRefCount() == 1) {
+                SharedContextData.callContextInitCallbacks(sharedContextData, gl);
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        for (GLImageListViewCellViewer v : instances) {
+                            if (v != GLImageListViewCellViewer.this) {
+                                v.createGlCanvas();
+                            }
+                        }
+                    }
+                });
+            }
         }
 
         @Override
