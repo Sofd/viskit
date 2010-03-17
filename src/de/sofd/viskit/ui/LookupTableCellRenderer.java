afc668c1166ff9a0a0564b9a652582706227bc94
Olaf Klischat <olaf.klischat@sofd.de>
2010-03-17 17:17:17 +0000

    viskit: LookupTableCellRenderer maxWidth

--- src/de/sofd/viskit/ui/LookupTableCellRenderer.java
+++ src/de/sofd/viskit/ui/LookupTableCellRenderer.java
@@ -1,6 +1,7 @@
 package de.sofd.viskit.ui;
 
 import java.awt.Component;
+import java.awt.Dimension;
 
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -9,8 +10,25 @@ import javax.swing.ListCellRenderer;
 import de.sofd.viskit.model.LookupTable;
 
 public class LookupTableCellRenderer implements ListCellRenderer {
+    
+    private final int maxWidth;
+    
+    LookupTableCellRenderer() {
+        this(Integer.MAX_VALUE);
+    }
+    
+    LookupTableCellRenderer(int maxWidth) {
+        this.maxWidth = maxWidth;
+    }
 
-    private JLabel resultLabel = new JLabel();
+    private JLabel resultLabel = new JLabel() {
+        @Override
+        public Dimension getPreferredSize() {
+            Dimension sz = super.getPreferredSize();
+            sz.width = Math.min(maxWidth, sz.width);
+            return sz;
+        }
+    };
     
     @Override
     public Component getListCellRendererComponent(JList list, Object value,
