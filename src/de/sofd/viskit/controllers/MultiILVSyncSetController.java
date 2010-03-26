273027fa5be1ccd59ed9e27aeb2efbbf068a9418
Olaf Klischat <olaf.klischat@sofd.de>
2010-03-26 15:48:17 +0000

    viskit: MultiILVSyncSetController intermediate state

--- src/de/sofd/viskit/controllers/MultiILVSyncSetController.java
+++ src/de/sofd/viskit/controllers/MultiILVSyncSetController.java
@@ -1,12 +1,15 @@
 package de.sofd.viskit.controllers;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.IdentityHashMap;
 import java.util.Map;
 import java.util.Set;
 
+import de.sofd.util.IdentityHashSet;
 import de.sofd.viskit.ui.imagelist.JImageListView;
+import de.sofd.viskit.ui.imagelist.glimpl.JGLImageListView;
 
 /**
  * Controller that holds multiple, mutable sets of {@link JImageListView}s
@@ -30,16 +33,21 @@ public class MultiILVSyncSetController {
     protected final Set<Class<? extends MultiImageListViewController>> syncControllerClasses =
         new HashSet<Class<? extends MultiImageListViewController>>();
     
-    protected final Map<Object, SyncSet> syncSetsByKey = new IdentityHashMap<Object, SyncSet>();
+    protected final Map<Object, SyncSetImpl> syncSetsByKey = new IdentityHashMap<Object, SyncSetImpl>();
 
     /**
-     * Register a new MultiImageListViewController subclass. The implementation will ensure that
-     * an instance of this class is associated with each sync set.
+     * Register a new MultiImageListViewController subclass. The implementation
+     * will ensure that an instance of this class is associated with each sync
+     * set. The class must have a public no-args constructor.
      * 
      * @param <C>
      * @param clazz
      */
     public <C extends MultiImageListViewController> void addSyncControllerType(Class<C> clazz) {
+        for (SyncSetImpl ss : syncSetsByKey.values()) {
+            ss.addSyncController(clazz);
+        }
+        updateSyncControllers();
     }
 
     /**
@@ -48,10 +56,8 @@ public class MultiILVSyncSetController {
      * @author olaf
      */
     public static interface SyncSet {
-        Object getKey();
-
         /**
-         * set/add/removeList -- dynamically mutate the list of JImageListViews
+         * set/add/removeList -- dynamically modify the list of JImageListViews
          * in the sync set.
          * 
          * @param lists
@@ -90,8 +96,9 @@ public class MultiILVSyncSetController {
         Object getAttribute(String name);
 
         /**
-         * Set the list of JImageListViews of one of this sync set's {@link MultiImageListViewController}s
-         * to this sync set, or to an empty list
+         * Keep the list of JImageListViews of one of this sync set's
+         * {@link MultiImageListViewController}s in sync with this sync set, or
+         * empty it.
          * 
          * @param <C>
          * @param clazz
@@ -99,21 +106,33 @@ public class MultiILVSyncSetController {
          *            registered via
          *            {@link MultiILVSyncSetController#addSyncControllerType(Class)}
          *            )
-         * @param synced flag indicating whether to set the controller's list of JImageListViews to the
-         *        sync set (synced=true), or clear it
+         * @param synced
+         *            flag indicating whether to keep the controller's list of
+         *            JImageListViews in sync with the sync set (synced=true),
+         *            or clear it
          */
         <C extends MultiImageListViewController> void syncController(Class<C> clazz, boolean synced);
+
+        /**
+         * 
+         * @return the key that was used when adding this sync set to this
+         *         controller (see
+         *         {@link MultiILVSyncSetController#addSyncSet(Object)}).
+         */
+        Object getKey();
     }
 
     /**
-     * add a new sync set.
+     * Add a new sync set. The sync set is created internally and returned.
      * 
      * @param key arbitrary key used to retrieve the set later
      * @return the newly created sync set
      */
     public SyncSet addSyncSet(Object key) {
-        SyncSetImpl syncSet = new SyncSetImpl();
-        
+        SyncSetImpl syncSet = new SyncSetImpl(key);
+        for (Class<? extends MultiImageListViewController> syncControllerClass : syncControllerClasses) {
+            syncSet.addSyncController(syncControllerClass);
+        }
         syncSetsByKey.put(key, syncSet);
         return syncSet;
     }
@@ -122,54 +141,124 @@ public class MultiILVSyncSetController {
         return syncSetsByKey.get(key);
     }
 
-    protected static class SyncSetImpl implements SyncSet {
+    protected class SyncSetImpl implements SyncSet {
+
+        protected final Object key;
+        protected final Set<JImageListView> lists = new IdentityHashSet<JImageListView>();
+        protected final Map<Class<? extends MultiImageListViewController>, MultiImageListViewController> syncControllersByClass
+            = new HashMap<Class<? extends MultiImageListViewController>, MultiImageListViewController>();
+        protected final Map<MultiImageListViewController, Boolean> isSyncedFlagByController = new HashMap<MultiImageListViewController, Boolean>();
+        protected final Map<String, Object> attrs = new HashMap<String, Object>();
+        
+        public SyncSetImpl(Object key) {
+            this.key = key;
+        }
 
         @Override
         public Object getKey() {
-            throw new UnsupportedOperationException();
+            return key;
         }
 
         @Override
         public void addList(JImageListView list) {
-            throw new UnsupportedOperationException();
+            lists.add(list);
+            updateSyncControllers();
         }
         
         @Override
         public void removeList(JImageListView list) {
-            throw new UnsupportedOperationException();
+            disassociateControllers();
+            lists.remove(list);
+            updateSyncControllers();
         }
 
         @Override
         public void setLists(Collection<JImageListView> lists) {
-            throw new UnsupportedOperationException();
+            disassociateControllers();
+            this.lists.clear();
+            this.lists.addAll(lists);
+            updateSyncControllers();
         }
 
         @Override
         public void setLists(JImageListView[] lists) {
-            throw new UnsupportedOperationException();
+            disassociateControllers();
+            this.lists.clear();
+            for (JImageListView l : lists) {
+                this.lists.add(l);
+            }
+            updateSyncControllers();
         }
 
         @Override
         public Object getAttribute(String name) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public <C extends MultiImageListViewController> C getSyncController(Class<C> clazz) {
-            throw new UnsupportedOperationException();
+            return attrs.get(name);
         }
 
         @Override
         public void setAttribute(String name, Object value) {
-            throw new UnsupportedOperationException();
+            attrs.put(name, value);
+        }
+
+        public <C extends MultiImageListViewController> void addSyncController(Class<C> clazz) {
+            try {
+                if (null != syncControllersByClass.get(clazz)) {
+                    throw new IllegalStateException("trying to add more than one " + clazz + " to " + MultiILVSyncSetController.class);
+                }
+                C controller = clazz.newInstance();
+                syncControllersByClass.put(clazz, controller);
+                isSyncedFlagByController.put(controller, false);
+            } catch (InstantiationException e) {
+                throw new IllegalArgumentException(e);
+            } catch (IllegalAccessException e) {
+                throw new IllegalArgumentException(e);
+            }
+        }
+        
+        @SuppressWarnings("unchecked")
+        @Override
+        public <C extends MultiImageListViewController> C getSyncController(Class<C> clazz) {
+            return (C) syncControllersByClass.get(clazz);
         }
 
         @Override
-        public <C extends MultiImageListViewController> void syncController(
-                Class<C> clazz, boolean synced) {
-            throw new UnsupportedOperationException();
+        public <C extends MultiImageListViewController> void syncController(Class<C> clazz, boolean synced) {
+            C controller = getSyncController(clazz);
+            if (null == controller) {
+                throw new IllegalArgumentException("unregistered MultiImageListViewController class: " + clazz);
+            }
+            isSyncedFlagByController.put(controller, synced);
+            updateSyncControllers();
         }
         
+        public <C extends MultiImageListViewController> boolean isControllerSynced(Class<C> clazz) {
+            C controller = getSyncController(clazz);
+            if (null == controller) {
+                throw new IllegalArgumentException("unregistered MultiImageListViewController class: " + clazz);
+            }
+            return isSyncedFlagByController.get(controller);
+        }
+        
+        protected void disassociateControllers() {
+            for (MultiImageListViewController c : syncControllersByClass.values()) {
+                c.setLists(new JGLImageListView[0]);
+            }
+        }
     }
     
+    protected void updateSyncControllers() {
+        for (SyncSetImpl ss : syncSetsByKey.values()) {
+            for (Class<? extends MultiImageListViewController> scc : syncControllerClasses) {
+                if (ss.isControllerSynced(scc)) {
+                    ss.getSyncController(scc).setLists(ss.lists);
+                } else {
+                    ss.getSyncController(scc).setLists(new JImageListView[0]);
+                }
+            }
+        }
+    }
+    
+
 }
+
+// TODO: is the idea of having this class a case of over-engineering?
