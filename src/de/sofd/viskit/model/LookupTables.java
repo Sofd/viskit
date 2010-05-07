ba1f09aec541419aa02060144030a5a7b1c72241
Olaf Klischat <olaf.klischat@sofd.de>
2010-05-07 11:29:10 +0000

    viskit: previous LUTs re-added

--- src/de/sofd/viskit/model/LookupTables.java
+++ src/de/sofd/viskit/model/LookupTables.java
@@ -15,6 +15,8 @@ public class LookupTables {
         try {
             LutController.init("img/luts/osx");
             LutController.loadFiles();
+            LutController.init("img/luts");
+            LutController.loadFiles();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
