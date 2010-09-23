5da79e9a3c87c9302f19bf8f5fa58db0fdedea93
Olaf Klischat <olaf.klischat@sofd.de>
2010-09-23 20:54:50 +0000

    viskit: svn2hg splicemap update 10 + additional tweaks

--- admin/svn2hg/convert.sh
+++ admin/svn2hg/convert.sh
@@ -3,5 +3,18 @@
 
 set -e
 
-./convert_svn2raw.sh
-./convert_raw2final.sh
+#./convert_svn2raw.sh
+#./convert_raw2final.sh
+
+
+#remove wrongly created ui-lib-neutrality-refactoring-pp branch
+
+. ./hgutils.sh
+
+sha1="`tosha1 'viskit: ui-lib-neutrality-refactoring-pp branch created' de.sofd.viskit/`"
+if [ "`echo -n $sha1 | wc -c`" -ne 40 ]; then
+    echo "no unique sha1 found for the wrongly created ui-lib-neutrality-refactoring-pp svn branch??"
+else
+    echo "removing wrongly created ui-lib-neutrality-refactoring-pp svn branch"
+    (set -e; cd de.sofd.viskit; hg strip "$sha1")
+fi
